146bc5c9a058c7d2d36f4868ccb199bbf9c93132
HURKAPARIZSI <kocsiskornel800@gmail.com>
2024-11-20 11:30:04 +0100

    .

--- modell/munkavallalo.php
+++ modell/munkavallalo.php
@@ -10,21 +10,27 @@ class Munkavallalok{
 
     public function __construct(int $id){
         try{
-        $kapcs = connect();
-        //biztonsági lépések
-        $this->id = (int)mysqli_real_escape_string($kapcs, $id);
-        $this->id = strip_tags($this->id);
-        //SQL parancs összerakása
-        $sql = 'CALL read('.$this->id.')';
-        //SQL parancs lefuttatása
-        $adatok = mysqli_query( $kapcs,$sql);
-        //Visszajött adatok kezelése
-        $fetcheltAdatok = mysqli_fetch_assoc($adatok);
-        //$fetcheltAdatok['id'] $fetcheltAdatok['nev'].....
-        $this->adojel = $fetcheltAdatok['adojel'];//NAGYON FONTOS!!!!!
-        $this->nev = $fetcheltAdatok['nev'];//NAGYON FONTOS!!!!!
-        $this->szulDatum = $fetcheltAdatok['szuldatum'];//NAGYON FONTOS!!!!!
-        mysqli_close($kapcs);
+            $kapcs = connect();
+
+            //biztonsági lépések
+            $this->id = (int)mysqli_real_escape_string($kapcs, $id);
+            $this->id = strip_tags($this->id);
+
+            //SQL parancs összerakása
+            $sql = 'CALL read('.$this->id.')';
+
+            //SQL parancs lefuttatása
+            $adatok = mysqli_query( $kapcs,$sql);
+
+            //Visszajött adatok kezelése
+            $fetcheltAdatok = mysqli_fetch_assoc($adatok);
+
+            //$fetcheltAdatok['id'] $fetcheltAdatok['nev'].....
+            $this->adojel = $fetcheltAdatok['adojel'];//NAGYON FONTOS!!!!!
+            $this->nev = $fetcheltAdatok['nev'];//NAGYON FONTOS!!!!!
+            $this->szulDatum = $fetcheltAdatok['szuldatum'];//NAGYON FONTOS!!!!!
+            
+            mysqli_close($kapcs);
 
         }
         catch(Exception $ex){
