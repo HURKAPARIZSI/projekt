25951bc6070dc5925fb6129453fceb13f73d7719
HURKAPARIZSI <kocsiskornel800@gmail.com>
2024-11-20 11:41:35 +0100

    .

--- modell/munkavallalo.php
+++ modell/munkavallalo.php
@@ -1,49 +1,41 @@
 <?php
 require_once('modelbase.php');
 
-class Munkavallalok{
+class Munkavallalok {
 
-    private string $nev;
-    private string $adojel;
-    private string $szulDatum;
-    private int $id;
+    private array $records = [];
 
-    public function __construct(int $id){
-        try{
+    public function __construct() {
+        try {
+            
             $kapcs = connect();
 
-            //biztonsági lépések
-            $this->id = (int)mysqli_real_escape_string($kapcs, $id);
-            $this->id = strip_tags($this->id);
+            // SQL parancs összerakása
+            $sql = 'CALL getAll()';
 
-            //SQL parancs összerakása
-            $sql = 'CALL read('.$this->id.')';
+            // SQL parancs lefuttatása
+            $adatok = mysqli_query($kapcs, $sql);
 
-            //SQL parancs lefuttatása
-            $adatok = mysqli_query( $kapcs,$sql);
+            // Visszajött adatok feldolgozása
+            while ($sor = mysqli_fetch_assoc($adatok)) {
+                $this->records[] = [
+                    'id' => (int)$sor['id'],
+                    'nev' => $sor['nev'],
+                    'adojel' => $sor['adojel'],
+                    'szulDatum' => $sor['szuldatum']
+                ];
+            }
 
-            //Visszajött adatok kezelése
-            $fetcheltAdatok = mysqli_fetch_assoc($adatok);
-
-            //$fetcheltAdatok['id'] $fetcheltAdatok['nev'].....
-            $this->adojel = $fetcheltAdatok['adojel'];//NAGYON FONTOS!!!!!
-            $this->nev = $fetcheltAdatok['nev'];//NAGYON FONTOS!!!!!
-            $this->szulDatum = $fetcheltAdatok['szuldatum'];//NAGYON FONTOS!!!!!
             
             mysqli_close($kapcs);
 
-        }
-        catch(Exception $ex){
+        } catch (Exception $ex) {
             print $ex->getMessage();
         }
     }
 
-
+    // Metódus az adatok lekérésére
+    public function getAll(): array {
+        return $this->records;
+    }
 }
-
-
-
-
-
-
-?>
