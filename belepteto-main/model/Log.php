1df87a12f8c4db7f3d8d027554f4c703fb652da2
HURKAPARIZSI <kocsiskornel800@gmail.com>
2025-02-05 09:46:27 +0100

    .

--- belepteto-main/model/Log.php
+++ belepteto-main/model/Log.php
@@ -2,15 +2,42 @@
 require_once 'ModelBase.php';
 
 class Log extends ModelBase {
-    public function logEvent($munkavallalo_id, $esemeny) {
-        $stmt = $this->pdo->prepare("INSERT INTO log (munkavallalo_id, esemeny, datum) VALUES (:munkavallalo_id, :esemeny, NOW())");
-        return $stmt->execute(['munkavallalo_id' => $munkavallalo_id, 'esemeny' => $esemeny]);
+    
+    public function createLog($userId, $message) {
+        $stmt = $this->pdo->prepare("CALL CreateLog(:userId, :message)");
+        return $stmt->execute([
+            'userId' => $userId,
+            'message' => $message
+        ]);
     }
 
-    public function getLogsByMunkavallaloId($munkavallalo_id) {
-        $stmt = $this->pdo->prepare("SELECT * FROM log WHERE munkavallalo_id = :munkavallalo_id ORDER BY datum DESC");
-        $stmt->execute(['munkavallalo_id' => $munkavallalo_id]);
+    public function getLogById($id) {
+        $stmt = $this->pdo->prepare("CALL GetLogById(:id)");
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllLogs() {
+        $stmt = $this->pdo->query("CALL GetAllLogs()");
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function updateLog($id, $message) {
+        $stmt = $this->pdo->prepare("CALL UpdateLog(:id, :message)");
+        return $stmt->execute([
+            'id' => $id,
+            'message' => $message
+        ]);
+    }
+
+    public function softDeleteLog($id) {
+        $stmt = $this->pdo->prepare("CALL SoftDeleteLog(:id)");
+        return $stmt->execute(['id' => $id]);
+    }
+
+    public function hardDeleteLog($id) {
+        $stmt = $this->pdo->prepare("CALL HardDeleteLog(:id)");
+        return $stmt->execute(['id' => $id]);
     }
 }
 ?>
